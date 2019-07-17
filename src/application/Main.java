9ed977acf85a0cd05cac683c8979f1532a18d9d8
田淵健太 <>
2019-07-17 14:47:08 +0900

    Comfirm it can read index.fxml.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -10,11 +11,11 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/index.fxml"));
+			Scene scene = new Scene((BorderPane)fxmlLoader.load());
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			new SalesDao(new SQLReadTest());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("目次");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -25,3 +26,13 @@ public class Main extends Application {
 		launch(args);
 	}
 }
+
+//FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/TradeLogTableStage.fxml"));
+//Scene scene = new Scene((BorderPane)fxmlLoader.load());
+//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//primaryStage.setScene(scene);
+//primaryStage.show();
+//primaryStage.setWidth(1100);
+//primaryStage.setX(primaryStage.getX());
+//primaryStage.setTitle("TradeLogTableStage");
+//tradeLogTableStageController = fxmlLoader.getController();
