52fd766f8f3e730f4e6aaa4c9df5022c9a282c6c
田淵健太 <>
2019-07-17 16:47:23 +0900

    Create PaneGenerator.

--- src/command/StageGenerator.java
+++ src/command/StageGenerator.java
@@ -5,20 +5,21 @@ import java.io.IOException;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class StageGenerator {
-	public Stage createStage(String fxmlFileName,String title){
+	public Stage createBoderPaneStage(String fxmlFileName,Pane pane){
 		Stage stage = new Stage();
-		BorderPane root;
+	
 		try {
-			root = (BorderPane)FXMLLoader.load(getClass().getResource("/fxml/" + fxmlFileName));
-			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/"+ fxmlFileName));
+			pane = (BorderPane) loader.load();
+			Scene scene = new Scene(pane);
 			stage.setScene(scene);
 			stage.show();
-			stage.setTitle(title);
 			return stage;
+
 		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
--- src/controller/IndexController.java
+++ src/controller/IndexController.java
@@ -1,11 +1,7 @@
 package controller;
 
-import java.io.IOException;
-
 import command.StageGenerator;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -13,32 +9,17 @@ public class IndexController {
 	@FXML
 	protected void OnMenuButton1Click(){
 		System.out.println("index.fxml:menu_button_1 clicked");
-		createBoderPaneStage("sales_menu.fxml");
-		
+		Stage stage = new StageGenerator().createBoderPaneStage("sales_menu.fxml",new BorderPane());
+		stage.setTitle("売上メニュー");
 	}
 	@FXML
 	protected void OnMenuButton2Click(){
-		System.out.println("index.fxml:menu_button_ clicked");
+		System.out.println("index.fxml:menu_button2_ clicked");
 	}
 	@FXML
 	protected void OnMenuButton3Click(){
-		System.out.println("index.fxml:menu_button_1 clicked");
+		System.out.println("index.fxml:menu_button_3 clicked");
 	}
 	
-	private Stage createBoderPaneStage(String fxmlFileName){
-		Stage stage = new Stage();
-	
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/"+ fxmlFileName));
-			BorderPane root = (BorderPane) loader.load();
-			Scene scene = new Scene(root);
-			stage.setScene(scene);
-			stage.show();
-			return stage;
 
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
 }
