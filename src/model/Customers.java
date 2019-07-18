84667d7331c7dd5a30c766c7ceecb0f43bd5c96a
田淵健太 <>
2019-07-18 14:13:25 +0900

    Customers.java change for tableview.

--- src/model/Customers.java
+++ src/model/Customers.java
@@ -1,88 +1,63 @@
 package model;
 
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class Customers {
-	private Long id;
-	private String name;
-	private String name_kana;
-	private String tel;
-	private String address1;
-	private String address2;
-	private String address3;
-	private String zip;
-	private String email;
-	private String person_in_charge;
-	private String created_at;
-	private String update_at;
-	public Long getId() {
+	private LongProperty id;
+	private StringProperty name;
+	private StringProperty name_kana;
+	private StringProperty tel;
+	private StringProperty address1;
+	private StringProperty address2;
+	private StringProperty address3;
+	private StringProperty zip;
+	private StringProperty email;
+	private StringProperty person_in_charge;
+	private StringProperty created_at;
+	private StringProperty update_at;
+
+	public Customers(Long id,String name){
+		this.id = new SimpleLongProperty(id);
+		this.name = new SimpleStringProperty(name);
+	}
+	
+	public  LongProperty idProperty(){
 		return id;
 	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
+	public StringProperty nameProperty(){
 		return name;
 	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getName_kana() {
+	public StringProperty name_kanaProperty(){
 		return name_kana;
 	}
-	public void setName_kana(String name_kana) {
-		this.name_kana = name_kana;
-	}
-	public String getTel() {
+	public StringProperty tesProperty(){
 		return tel;
 	}
-	public void setTel(String tel) {
-		this.tel = tel;
-	}
-	public String getAddress1() {
+	public StringProperty address1Property(){
 		return address1;
 	}
-	public void setAddress1(String address1) {
-		this.address1 = address1;
-	}
-	public String getAddress2() {
+	public StringProperty address2Property(){
 		return address2;
 	}
-	public void setAddress2(String address2) {
-		this.address2 = address2;
-	}
-	public String getAddress3() {
+	public StringProperty address3Property(){
 		return address3;
 	}
-	public void setAddress3(String address3) {
-		this.address3 = address3;
-	}
-	public String getZip() {
+	public StringProperty zipProperty(){
 		return zip;
 	}
-	public void setZip(String zip) {
-		this.zip = zip;
-	}
-	public String getEmail() {
+	public StringProperty emailProperty(){
 		return email;
 	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPerson_in_charge() {
+	public StringProperty person_in_chargeProperty(){
 		return person_in_charge;
 	}
-	public void setPerson_in_charge(String person_in_charge) {
-		this.person_in_charge = person_in_charge;
-	}
-	public String getCreated_at() {
+	public StringProperty created_atProperty(){
 		return created_at;
 	}
-	public void setCreated_at(String created_at) {
-		this.created_at = created_at;
-	}
-	public String getUpdate_at() {
+	public StringProperty update_atProperty(){
 		return update_at;
-	}
-	public void setUpdate_at(String update_at) {
-		this.update_at = update_at;
 	}
 }
