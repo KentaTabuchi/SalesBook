8e6ec0c65fea0f8933f6f58d9ef484d09b4870e1
田淵健太 <>
2019-07-27 21:42:48 +0900

    temp2

--- src/controller/SalesDetailEditController.java
+++ src/controller/SalesDetailEditController.java
@@ -1,11 +1,9 @@
 package controller;
 
-import application.SalesDao;
-import sql_calc.Sales_Max_Id;
-
 public class SalesDetailEditController extends SalesDetailCRUDController {
 	@Override
 	public void setSales_id(){
-		sales_id = 34L;
+		
+		sales_id = SalesTableUController.sales.idProperty().getValue();
 	}
 }
