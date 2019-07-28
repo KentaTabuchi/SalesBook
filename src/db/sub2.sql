616dab21b3ae8ab0dcbd28f79a2eb5a01b85d550
田淵健太 <>
2019-07-28 14:12:18 +0900

    temp1

--- src/db/sub2.sql
+++
@@ -1,6 +0,0 @@
-select
-*,
-(select statuses.name from sales left outer join statuses on sales.staff_system_id = statuses.id)as staff_system_name,
-(select statuses.name  from sales left outer join statuses on sales.staff_design_id = statuses.id)as staff_design_name 
-from sales
-;
--- src/db/test1.sql
+++
@@ -1,8 +0,0 @@
-select *,customers.name 
-from sales_detail
-  left outer join
-  customers
-  on
-  sales_detail.vendor_id = customers.id
-  where
-  sales_detail.sales_id = 10
